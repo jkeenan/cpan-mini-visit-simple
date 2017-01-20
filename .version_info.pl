d4fc904fc5c9e1db8f8a20efce3ac59d7f4b4e6c
James E Keenan <jkeenan@cpan.org>
2017-01-19 19:49:53 -0500

    Add .version_info.pl.

---
+++ .version_info.pl
@@ -0,0 +1,8 @@
+%trans = (
+    version => { old => '0.009', new => '0.010' },
+    date    => { old => 'July 12 2015', new => 'January 29 2017' },
+    copyright => { old => '2015', new => '2017' },
+);
+
+1;
+
