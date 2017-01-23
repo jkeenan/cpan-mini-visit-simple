e5b66366e18ab8980927b57e1b911c8f86f67cb1
James E Keenan <jkeenan@cpan.org>
2017-01-23 13:27:37 -0500

    CPAN version 0.013
    Update version numbers, release dates, Changes.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.011', new => '0.012' },
-    date    => { old => 'January 19 2017', new => 'January 20 2017' },
+    version => { old => '0.012', new => '0.013' },
+    date    => { old => 'January 20 2017', new => 'January 23 2017' },
     copyright => { old => '2017', new => '2017' },
 );
 
