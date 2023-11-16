094caba02cdb3e98f4f3a24fd023c7a3247c5b5f
James E Keenan <jkeenan@cpan.org>
2023-11-16 18:01:42 -0500

    CPAN version 0.018
    Increment version numbers.  Update Changes.

--- .version_info.pl
+++ .version_info.pl
@@ -1,7 +1,7 @@
 %trans = (
-    version => { old => '0.016', new => '0.017' },
-    date    => { old => 'November 20 2017', new => 'November 25 2017' },
-    copyright => { old => '2017', new => '2017' },
+    version => { old => '0.017', new => '0.018' },
+    date    => { old => 'November 25 2017', new => 'November 16 2023' },
+    copyright => { old => '2017', new => '2023' },
 );
 
 1;
